359f4b8afce4c21f5a54f7eda53d5b67918a8898
Matt Nightingale <matt.snail@gmail.com>
2025-10-01 21:40:18 +0100

    adding js functionality

--- api.js
+++ api.js
@@ -4,22 +4,13 @@ function randomRecipe() {
         .then((res) => res.json())
         .then((data) => {
             console.log('data:', data)
-            displayRandomRecipe(data)
+            createRandomCard(data)
+            const header = document.querySelector('.header')
+            header.backgroundImage = `url(${data.meals[0].strMealThumb})`
         })
         .catch((error) => {
             console.error('Error fetching data:', error)
         })
-}
-
-function displayRandomRecipe(data) {
-    var recipeElement = document.querySelector('.recipetitle')
-    recipeElement.textContent = data.meals[0].strMeal
-
-    var imageElement = document.querySelector('.recipeimage')
-    imageElement.src = data.meals[0].strMealThumb
-
-    var instructionsElement = document.querySelector('.recipeinstructions')
-    instructionsElement.textContent = data.meals[0].strInstructions
 }
 
 function searchRecipe(e) {
@@ -28,17 +19,19 @@ function searchRecipe(e) {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`
     fetch(url)
         .then((res) => res.json())
-        .then((data) => {
-            console.log('data:', data)
+        .then((searchData) => {
+            console.log('data:', searchData)
             console.log(term)
-            displaySearchResults(data)
+            createRecipeCard(searchData)
         })
         .catch((error) => {
             console.error('Error fetching data:', error)
         })
+        removeRandomRecipe()
+        removeSearchRecipe()
 }
 
-function displaySearchResults(data) {
+function createRandomCard(data) {
     var recipeElement = document.querySelector('.recipetitle')
     recipeElement.textContent = data.meals[0].strMeal
 
@@ -47,6 +40,33 @@ function displaySearchResults(data) {
 
     var instructionsElement = document.querySelector('.recipeinstructions')
     instructionsElement.textContent = data.meals[0].strInstructions
+}
+
+function createRecipeCard(searchData) {
+    searchData.meals.forEach((meal) => {
+        var card = document.createElement('div')
+        card.classList.add('searchrecipe')
+        document.body.appendChild(card)
+        var newTitle = document.createElement('h2')
+        newTitle.textContent = meal.strMeal
+        card.appendChild(newTitle)
+        var newimage = document.createElement('img')
+        newimage.src = meal.strMealThumb
+        card.appendChild(newimage)
+        var newInstructions = document.createElement('p')
+        newInstructions.textContent = meal.strInstructions
+        card.appendChild(newInstructions)
+    })
+}
+
+function removeRandomRecipe() {
+    const removal = document.querySelector('.randomrecipe')
+    removal.remove('randomRecipe')
+}
+
+function removeSearchRecipe() {
+    const removal = document.querySelector('.searchrecipe')
+    removal.remove('searchRecipe')
 }
 
 const searchButton = document.querySelector('.searchbutton')
